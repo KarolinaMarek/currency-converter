291bd00de069520ad3e6277945b8dc2aa8f6b1f6
Karolina Marek <karolinaw1211@gmail.com>
2020-07-20 17:17:36 +0200

    refactored code

--- js/script.js
+++ js/script.js
@@ -1,101 +1,99 @@
-let choicePln = document.querySelector(".js-formChoice__input--pln");
-let choiceOtherCurrency = document.querySelector(".js-formChoice__input--otherCurrency");
-let formPln = document.querySelector(".js-form--pln");
-let formOtherCurrency = document.querySelector(".js-form--otherCurrency");
+{
+    const choicePln = document.querySelector(".js-formChoice__input--pln");
+    const choiceOtherCurrency = document.querySelector(".js-formChoice__input--otherCurrency");
+    const formPln = document.querySelector(".js-form--pln");
+    const formOtherCurrency = document.querySelector(".js-form--otherCurrency");
 
-choicePln.addEventListener("click", () => {
-    if (choicePln.checked) {
-        formPln.classList.remove("form--displayNone");
-        formOtherCurrency.classList.add("form--displayNone");
-    }
-});
+    choicePln.addEventListener("click", () => {
+        if (choicePln.checked) {
+            formPln.classList.remove("form--displayNone");
+            formOtherCurrency.classList.add("form--displayNone");
+        }
+    });
 
-choiceOtherCurrency.addEventListener("click", () => {
-    if (choiceOtherCurrency.checked) {
-        formPln.classList.add("form--displayNone");
-        formOtherCurrency.classList.remove("form--displayNone");
-    }
-});
+    choiceOtherCurrency.addEventListener("click", () => {
+        if (choiceOtherCurrency.checked) {
+            formPln.classList.add("form--displayNone");
+            formOtherCurrency.classList.remove("form--displayNone");
+        }
+    });
 
-let rateEuro = (2 + Math.random()).toFixed(3);
-let rateUsd = (3 + Math.random()).toFixed(3);
-let rateGbp = (4 + Math.random()).toFixed(3);
-let rateChf = (5 + Math.random()).toFixed(3);
+    const rate = number => (number + Math.random()).toFixed(3);
 
+    const rateEuro = rate(2);
+    const rateUsd = rate(3);
+    const rateGbp = rate(4);
+    const rateChf = rate(5);
 
-let formElementPln = document.querySelector(".js-form__input--plnAmount");
-let formSelectPln = document.querySelector(".js-form__select--pln");
-let currencyEuro = document.querySelector(".js-form__option--euro");
-let currencyUsd = document.querySelector(".js-form__option--usd");
-let currencyGbp = document.querySelector(".js-form__option--gbp");
-let currencyChf = document.querySelector(".js-form__option--chf");
-let finaleResultPln = document.querySelector(".js-form__amountFinal--pln");
-let elementCurrency = document.querySelector(".js-form__currency--pln");
+    formPln.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formElementPln = document.querySelector(".js-form__input--plnAmount");
+        const formSelectPln = document.querySelector(".js-form__select--pln");
+        const finaleResultPln = document.querySelector(".js-form__amountFinal--pln");
+        const elementCurrency = document.querySelector(".js-form__currency--pln");
+        const currency = formSelectPln.value;
+        const finaleResult = (rate) => {
+            const amountPln = formElementPln.value;
+            elementCurrency.innerText = currency;
+            finaleResultPln.innerText = (amountPln / rate).toFixed(2);
+        };
+        switch (currency) {
+            case "EURO":
+                finaleResult(rateEuro);
+                break;
+            case "USD":
+                finaleResult(rateUsd);
+                break;
+            case "GBP":
+                finaleResult(rateGbp);
+                break;
+            case "CHF":
+                finaleResult(rateChf);
+                break;
+        };
+    });
 
+    formOtherCurrency.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formSelectOtherCurrency = document.querySelector(".js-form__select--otherCurrency");
+        const formElementOtherCurrency = document.querySelector(".js-form__input--otherCurrencyAmount");
+        const finaleResultOtherCurrency = document.querySelector(".js-form__amountFinal--otherCurrency");
+        const currency = formSelectOtherCurrency.value;
 
+        const finaleResult = (rate) => {
+            const amountOtherCurrency = formElementOtherCurrency.value;
+            const final = (amountOtherCurrency * rate).toFixed(2);
+            finaleResultOtherCurrency.innerText = final;
+        };
+        switch (currency) {
+            case "EURO":
+                finaleResult(rateEuro);
+                break;
+            case "USD":
+                finaleResult(rateUsd);
+                break;
+            case "GBP":
+                finaleResult(rateGbp);
+                break;
+            case "CHF":
+                finaleResult(rateChf);
+                break;
+        };
+    });
 
-formPln.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amountPln = formElementPln.value;
-    switch (formSelectPln.value) {
-        case currencyEuro.value:
-            elementCurrency.innerText = currencyEuro.innerText;
-            finaleResultPln.innerText = (amountPln / rateEuro).toFixed(2);
-            break;
-        case currencyUsd.value:
-            elementCurrency.innerText = currencyUsd.innerText;
-            finaleResultPln.innerText = (amountPln / rateUsd).toFixed(2);
-            break;
-        case currencyGbp.value:
-            elementCurrency.innerText = currencyGbp.innerText;
-            finaleResultPln.innerText = (amountPln / rateGbp).toFixed(2);
-            break;
-        case currencyChf.value:
-            elementCurrency.innerText = currencyChf.innerText;
-            finaleResultPln.innerText = (amountPln / rateChf).toFixed(2);
-            break;
+    const rateWritten = (rateWritten, rate) => {
+        rateWritten.innerText = rate;
     };
-});
+
+    const rateWrittenEuro = document.querySelector(".js-currencyRate__value--euro");
+    const rateWrittenUsd = document.querySelector(".js-currencyRate__value--usd");
+    const rateWrittenGbp = document.querySelector(".js-currencyRate__value--gbp");
+    const rateWrittenChf = document.querySelector(".js-currencyRate__value--chf");
+
+    rateWritten(rateWrittenEuro, rateEuro);
+    rateWritten(rateWrittenUsd, rateUsd);
+    rateWritten(rateWrittenGbp, rateGbp);
+    rateWritten(rateWrittenChf, rateChf);
 
 
-let formSelectOtherCurrency = document.querySelector(".js-form__select--otherCurrency");
-let formElementOtherCurrency = document.querySelector(".js-form__input--otherCurrencyAmount");
-let currencyEuro2 = document.querySelector(".js-form__option--euro2");
-let currencyUsd2 = document.querySelector(".js-form__option--usd2");
-let currencyGbp2 = document.querySelector(".js-form__option--gbp2");
-let currencyChf2 = document.querySelector(".js-form__option--chf2");
-let finaleResultOtherCurrency = document.querySelector(".js-form__amountFinal--otherCurrency");
-
-formOtherCurrency.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amountOtherCurrency = formElementOtherCurrency.value;
-    switch (formSelectOtherCurrency.value) {
-        case currencyEuro2.value:
-            finaleResultOtherCurrency.innerText = (amountOtherCurrency * rateEuro).toFixed(2);
-            break;
-        case currencyUsd2.value:
-            finaleResultOtherCurrency.innerText = (amountOtherCurrency * rateUsd).toFixed(2);
-            break;
-        case currencyGbp.value:
-            finaleResultOtherCurrency.innerText = (amountOtherCurrency * rateGbp).toFixed(2);
-            break;
-        case currencyChf.value:
-            finaleResultOtherCurrency.innerText = (amountOtherCurrency * rateChf).toFixed(2);
-            break;
-    };
-});
-
-let rateWrittenEuro = document.querySelector(".js-currencyRate__value--euro");
-rateWrittenEuro.innerText = rateEuro;
-
-let rateWrittenUsd = document.querySelector(".js-currencyRate__value--usd");
-rateWrittenUsd.innerText = rateUsd;
-
-let rateWrittenGbp = document.querySelector(".js-currencyRate__value--gbp");
-rateWrittenGbp.innerText = rateGbp;
-
-let rateWrittenChf = document.querySelector(".js-currencyRate__value--chf");
-rateWrittenChf.innerText = rateChf;
-
-
-
+}
